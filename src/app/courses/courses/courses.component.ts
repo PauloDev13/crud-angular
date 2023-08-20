5eb8e46e8e1679223910a30d67757371861937c1
PgmAdmin <prmorais1302@gmail.com>
2023-08-19 22:36:58 -0300

    11 - List of Courses: Error Handling and MatDialog

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import {Course} from "./model/course";
 import {CoursesService} from "./services/courses.service";
-import {Observable} from "rxjs";
+import {Observable, of} from "rxjs";
+import {catchError } from "rxjs/operators";
+import {MatDialog} from "@angular/material/dialog";
+import {ErrorDialogComponent} from "../shared/components/error-dialog/error-dialog.component";
 
 @Component({
   selector: 'app-courses',
@@ -12,7 +15,22 @@ export class CoursesComponent {
   courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category']
 
-  constructor(private coursesService: CoursesService) {
-    this.courses$ = coursesService.list();
+  constructor(
+    public dialog: MatDialog,
+    private coursesService: CoursesService
+  ) {
+    this.courses$ = coursesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar curso.')
+          return of([]);
+        })
+      )
   }
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
+  }
+
 }
