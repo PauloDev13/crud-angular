70c335d4a7c6bbc5c0424525d6f70388524258ce
PgmAdmin <prmorais1302@gmail.com>
2023-08-19 22:55:58 -0300

    12 - List of Courses: Pipe to show icon

--- src/app/courses/courses/services/courses.service.ts
+++ src/app/courses/courses/services/courses.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 
 import {Course} from '../model/course';
 import {HttpClient} from '@angular/common/http';
-import {delay, first, tap} from 'rxjs/operators';
+import {first, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
-  private readonly API = '/assetss/courses.json';
+  private readonly API = '/assets/courses.json';
 
   constructor(private httpClient: HttpClient) {
   }
@@ -17,7 +17,7 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
       .pipe(
         first(),
-        delay(3000),
+        // delay(3000),
         tap(courses => console.log(courses)));
   }
 }
