1721a4931c1bae34f5810a85102dcf1ae6fb7b48
Paulo Roberto De Morais <prmorais1302@gmail.com>
2023-08-21 15:25:06 -0300

    25 - Smart Components x Presentation Components

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -5,9 +5,9 @@ import {ReactiveFormsModule} from "@angular/forms";
 import { CoursesRoutingModule } from './courses-routing.module';
 import {AppMaterialModule} from "./shared/app-material/app-material.module";
 import {SharedModule} from "./shared/shared.module";
-import { CoursesComponent } from './courses/courses.component';
-import { CourseFormComponent } from './course-form/course-form.component';
-import { CoursesListComponent } from './courses-list/courses-list.component';
+import { CoursesComponent } from './containers/courses/courses.component';
+import { CourseFormComponent } from './containers/course-form/course-form.component';
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
 
 
 @NgModule({
