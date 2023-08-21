81d9979b26c8d46c0cbb755a588b6c001e2f28c1
Paulo Roberto De Morais <prmorais1302@gmail.com>
2023-08-21 13:03:47 -0300

    21 - Form: API Spring: Create Course (HTTP POST)

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import {Location} from "@angular/common";
 import {FormBuilder, FormGroup} from "@angular/forms";
+
 import {CoursesService} from "../courses/services/courses.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
 
@@ -16,6 +18,7 @@ export class CourseFormComponent {
     private formBuilder: FormBuilder,
     private courseService: CoursesService,
     private snackBar: MatSnackBar,
+    private location: Location,
   ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -25,19 +28,23 @@ export class CourseFormComponent {
 
   onSubmit(){
     this.courseService.save(this.form.value).subscribe({
-      next: result => console.log(result),
+      next: () => this.onSuccess(),
       error: () => this.onError()
     })
 
   }
 
   onCancel(){
-    console.log('onCancel');
+    this.location.back();
+  }
+
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 3000 });
+    this.onCancel();
   }
 
   private onError() {
-    this.snackBar.open('Erro ao salvar curso.', '', {duration: 3000})
+    this.snackBar.open('Erro ao salvar curso.', '', { duration: 3000 });
   }
-
 
 }
