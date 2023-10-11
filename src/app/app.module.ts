1fd07c4585d9e01feb7fd8990908724c85215903
Paulo Roberto De Morais <prmorais1302@gmail.com>
2023-10-11 13:54:11 -0300

    Installing packages the NGRX and configuring files the store files in the store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +18,9 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     HttpClientModule,
     MatToolbarModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/store/course.action.ts
@@ -0,0 +1,4 @@
+// Declaration constants for actions load
+export const LOAD_COURSES = '[course page] load courses';
+export const LOAD_COURSE_SUCCESS = '[course page] load courses success';
+export const LOAD_COURSE_ERROR = '[course page] load courses error';
